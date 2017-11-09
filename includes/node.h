3241c1a598da7aede3ba5823407b43bdf3cf98a8
lukehayes <lukepurplehayes@gmail.com>
2017-11-09 20:38:13 +0000

    Added include guard to Node type

--- includes/node.h
+++ includes/node.h
@@ -4,8 +4,13 @@
 
 -------------------------------------------------------------------------------------*/
 
+#ifndef NODE_H
+#define NODE_H
+
 struct Node {
 	int data;
 	struct Node* left;
 	struct Node* right;
 };
+
+#endif
