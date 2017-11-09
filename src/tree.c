cd161eeb421b1b5c0ca90739b888cb312f598155
lukehayes <lukepurplehayes@gmail.com>
2017-11-09 20:59:37 +0000

    Updated tree.h

--- src/tree.c
+++ src/tree.c
@@ -1,10 +1,15 @@
 #include "tree.h"
+#include "node.h"
 #include <stdio.h>
 
 struct Node* tree(struct Node n ) {
-	struct Node current = NULL;
+	struct Node* current = NULL;
 
-	if( current == NULL ) {
-		current = n;
+	if( current == 0 ) {
+		current = &n;
 	}
+
+	printf("Current data: %i ", n.data);
+
+	return current;
 }
