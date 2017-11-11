4367d4bf05cf6b412d40bd94e5a35399f4e795be
lukehayes <lukepurplehayes@gmail.com>
2017-11-11 11:02:54 +0000

    Removed extra nodes

--- main.c
+++ main.c
@@ -6,20 +6,8 @@
 int main()
 {
 	struct Node* n1 = malloc(sizeof(struct Node));
-	n1->data = 6;
-
-	struct Node* n2 = malloc(sizeof(struct Node));
-	n2->data = 10;
-
-	struct Node* n3 = malloc(sizeof(struct Node));
-	n3->data = 3;
-
-	tree(*n1);
-
-
 
 	free(n1);
-	free(n2);
-	free(n3);
+
     return 0;
 }
