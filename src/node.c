deb103d6da79a63a708cc42e16814cc040625d87
lukehayes <lukepurplehayes@gmail.com>
2020-06-15 20:32:06 +0100

    Removed cpp contents from node.c

--- src/node.c
+++ src/node.c
@@ -1,17 +1,1 @@
 #include "node.h"
-
-Node::Node() :
-	data(0), next(nullptr)
-{}
-
-Node::Node(int data) :
-	data(data), next(nullptr)
-{}
-Node::~Node() {}
-
-Node* Node::getNext() {
-	return this->next;
-}
-void Node::setNext(Node* n) {
-	this->next = n;
-}
