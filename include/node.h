7d4280ea5b201ce58eb72de0d4b74b7ef1222a3c
lukehayes <lukepurplehayes@gmail.com>
2020-06-15 20:31:43 +0100

    Cleared header files

--- include/node.h
+++ include/node.h
@@ -7,27 +7,4 @@
 #ifndef NODE_H
 #define NODE_H
 
-class Node
-{
-public:
-	Node();
-	Node(int data);
-	~Node();
-
-	/**
-	 * Get the next node
-	 * @return [description]
-	 */
-	Node* getNext();
-
-	/**
-	 * Set the next Node
-	 * @param n [description]
-	 */
-	void setNext(Node* n);
-	int data;
-private:
-	Node* next;
-};
-
 #endif
