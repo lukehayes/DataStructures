a5161321939653c7794743a4c50ba5054fc631d3
lukehayes <lukepurplehayes@gmail.com>
2020-06-15 20:32:19 +0100

    Removed cpp contents from main.c

--- main.c
+++ main.c
@@ -1,13 +1,9 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "node.h"
 
 int main() {
-	Node n1(10);
-	Node n2(200);
-
-	n1.setNext(&n2);
-
-	std::cout << n1.getNext()->data << std::endl;
 
     return 0;
 }
