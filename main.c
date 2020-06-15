004559e58f2cc3cf2d07d61d7aeba9ebc746a2b8
lukehayes <lukepurplehayes@gmail.com>
2020-06-15 21:23:54 +0100

    Updated main

--- main.c
+++ main.c
@@ -5,5 +5,19 @@
 
 int main() {
 
+    Node* n1 = (Node*) malloc(sizeof(Node));
+    n1->value = 1;
+    Node* n2 = newNode(2, n1);
+    Node* n3 = newNode(10, n2);
+
+    printf("%i\n", n1->value);
+    printf("%i\n", n2->value);
+    printf("%i\n", n3->value);
+
+
+    free(n1);
+    free(n2);
+
+
     return 0;
 }
