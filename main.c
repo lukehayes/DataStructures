b3cddfee8a45fe1defca6864dc061fab474723ce
lukehayes <lukepurplehayes@gmail.com>
2020-06-16 14:58:25 +0100

    Updated main.c

--- main.c
+++ main.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "linked-list.h"
 #include "node.h"
 
 int main() {
 
-    Node* n1 = (Node*) malloc(sizeof(Node));
-    n1->value = 1;
-    Node* n2 = newNode(2, n1);
-    Node* n3 = newNode(10, n2);
+    List list = CreateList();
 
-    printf("%i\n", n1->value);
-    printf("%i\n", n2->value);
-    printf("%i\n", n3->value);
+    AddNode(&list, 1);
+    AddNode(&list, 2);
+    AddNode(&list, 3);
+    AddNode(&list, 4);
 
-
-    free(n1);
-    free(n2);
 
 
     return 0;
