21c00964bfda99f5ebbc898b00414411a46cbf3a
lukehayes <lukepurplehayes@gmail.com>
2020-06-16 14:58:11 +0100

    Implemented AddNode for Linked List

--- include/linked-list.h
+++ include/linked-list.h
@@ -18,4 +18,50 @@ typedef struct List {
 } List;
 
 
+/**
+ * Create a list with head and tail set to NULL
+ *
+ * @return List
+ */
+List CreateList() {
+    List list;
+    list.head = NULL;
+    list.tail = NULL;
+
+    return list;
+}
+
+
+/**
+ * Add a new node to the list struct
+ *
+ * @param List* list
+ * @param Node* lastNode
+ * @param int value
+ *
+ */
+void AddNode(List* list, int value) {
+
+    Node* node = (Node*) malloc(sizeof(Node));
+    node->value = value;
+
+    if(list->head == NULL) {
+        puts("Head is NULL so I will add Node to head.");
+        list->head = &node;
+    }else {
+        puts("Head set so moving on...");
+        Node* head = list->head;
+
+        while(head == NULL) {
+            head = head->next++;
+            printf("Searching... Value: %i\n", head->value);
+        }
+
+        head = node;
+            printf("Searching... Value: %i\n", head->value);
+        puts("Done");
+    }
+}
+
+
 #endif
