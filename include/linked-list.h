9a54194d3be2f3507c1a3dee0ca0261f59c2babe
lukehayes <lukepurplehayes@gmail.com>
2020-06-16 12:00:53 +0100

    Added doubly linked list struct

--- include/linked-list.h
+++ include/linked-list.h
@@ -1,5 +1,19 @@
+/**********************************************************************************************
+*
+*  Single and double linked lists are defined in this folder
+*
+**********************************************************************************************/
+
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
+
+/**
+ * Doubly Linked List
+ */
+typedef struct List {
+    Node* head;
+    Node* tail;
+} List;
 
 
 #endif
