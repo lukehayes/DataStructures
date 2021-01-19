77d1e0911f22a60785c69fb85af1edc147b4071d
lukehayes <lukepurplehayes@gmail.com>
2021-01-19 13:54:11 +0000

    Updated main with test code

--- main.c
+++ main.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "include/linked-list.h"
 #include "node.h"
-#include "log.h"
+
+void printNodes(Node* node)
+{
+    Node* temp = node;
+
+    while(temp->next != NULL)
+    {
+        printf("%i - ", temp->value);
+        temp = temp->next;
+    }
+}
+
+void addNode(Node* ptrNode, int value, Node* node)
+{
+    node = malloc(sizeof(Node));
+    node->value = value;
+    node->next = NULL;
+    ptrNode->next = node;
+}
+
 
 int main() {
 
-    l("Working");
-    ls("Working", "Again");
-    li("Working", 100);
+    Node* ptr;
+    Node a,b,c;
+    a.value = 3;
+    b.value = 2;
+    c.value = 1;
+
+
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+
+    addNode(&c, 100, ptr);
+
+
+    printNodes(&a);
+
+    printf("%i \n", ptr->value);
 
 
     return 0;
