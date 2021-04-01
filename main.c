87efc3a614070ae3ed4d1e9248113df798a13b41
lukehayes <lukepurplehayes@gmail.com>
2021-04-01 17:56:02 +0100

    Simplified main.c - refactored functions and made it cleaner.

--- main.c
+++ main.c
@@ -2,52 +2,34 @@
 #include <stdlib.h>
 #include "node.h"
 
-void printNodes(Node* node)
-{
-    Node* temp = node;
 
-    while(temp->next != NULL)
+void p(node_t* head)
+{
+    node_t* temp = head;
+
+    while(temp != NULL)
     {
-        printf("%i - ", temp->value);
+        printf("%i -", temp->value);
         temp = temp->next;
     }
-}
 
-Node* addNode(Node* ptrNode, int value)
-{
-    Node* node = malloc(sizeof(Node));
-    node->value = value;
-    node->next = NULL;
-    ptrNode->next = node;
-    
-    return node;
+    printf("\n");
 }
-
 
 int main() {
 
-    Node* ptr, *ptr2;
-    Node a,b,c;
-    a.value = 3;
-    b.value = 2;
-    c.value = 1;
+    struct node_t n1,n2,n3;
+
+    n1.value = 55;
+    n2.value = 803;
+    n3.value = 7;
+
+    n1.next = &n2;
+    n3.next = &n1;
+    n2.next = NULL;
 
 
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
-
-    ptr = addNode(&c, 100);
-    ptr2 = addNode(ptr, 234);
-
-
-    printNodes(&a);
-
-    printf("%i \n", ptr->value);
-
-    free(ptr);
-    free(ptr2);
-
+    p(&n3);
 
     return 0;
 }
