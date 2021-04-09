439cf0cf61b46b53d6a2e041e517ab6ed0043772
lukehayes <lukepurplehayes@gmail.com>
2021-04-09 14:45:06 +0100

    List struct is now an opaque type for better abstraction.

--- include/list.h
+++ include/list.h
@@ -1,15 +1,7 @@
 #ifndef DS_LIST_H
 #define DS_LIST_H
 
-#include <stddef.h>
-#include <stdio.h>
-#include "node.h"
-
-typedef struct List {
-    node_t* head;
-    node_t* tail;
-    int size;
-} List;
+typedef struct List_ List;
 
 List* CreateList();
 
@@ -21,17 +13,6 @@ void  DestroyList(List* list);
 /**
  * Output a formatted view of the list.
  */
-void PrintList(node_t* head)
-{
-    node_t* temp = head;
-
-    while(temp != NULL)
-    {
-        printf("%i -", temp->value);
-        temp = temp->next;
-    }
-
-    printf("\n");
-}
+void PrintList(node_t* head);
 
 #endif // DS_LIST_H
--- src/list.c
+++ src/list.c
@@ -1,5 +1,12 @@
 #include "list.h"
+#include "node.h"
 #include <stdlib.h>
+
+typedef struct List_ {
+    node_t* head;
+    node_t* tail;
+    int size;
+} List;
 
 List* CreateList()
 {
@@ -13,4 +20,17 @@ List* CreateList()
 void  DestroyList(List* list)
 {
     // TODO
+}
+
+void PrintList(node_t* head)
+{
+    node_t* temp = head;
+
+    while(temp != NULL)
+    {
+        printf("%i -", temp->value);
+        temp = temp->next;
+    }
+
+    printf("\n");
 }
