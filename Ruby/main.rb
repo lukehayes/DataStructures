434318d262e2a9a0bcc9b2a423a5df8ef0b2a489
Luke Hayes <lukepurplehayes@gmail.com>
2022-01-13 10:42:07 +0000

    Updated main.rb with latest test code.

--- Ruby/main.rb
+++ Ruby/main.rb
@@ -2,7 +2,26 @@
 $LOAD_PATH.unshift(Dir.pwd + "/DS/")
 
 require "list"
+require "list_node"
+require "printer/list_printer"
 
-l = DS::List.new
+lp = DS::Printer::ListPrinter.new
+
+a = DS::ListNode.new(100,nil)
+b = DS::ListNode.new(200,a)
+c = DS::ListNode.new(300,b)
+d = DS::ListNode.new(300,c)
+
+ll = DS::List.new(a)
+
+
+node = a
+
+loop do
+  puts "Printing "
+  puts node.value
+  node = node.next_node
+  break if node.nil?
+end
 
 
