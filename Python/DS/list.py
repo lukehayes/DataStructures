af2070b5dc0028f5cba206d54298002fb7af7909
Luke Hayes <lukepurplehayes@gmail.com>
2022-08-27 14:11:16 +0100

    Node constructor now accepts a Node instance.

--- Python/DS/list.py
+++ Python/DS/list.py
@@ -2,6 +2,8 @@
 class Node(object):
     def __init__(self, value):
         self.next = None
+    def __init__(self, value, next_node):
+        self.next = next_node
         self.value = value
 
 
