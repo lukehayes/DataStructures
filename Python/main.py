123ed206bfaae6054e2f66ed522097ab28f8e760
Luke Hayes <lukepurplehayes@gmail.com>
2022-08-28 10:06:07 +0100

    Updated main.py with test logic.

--- Python/main.py
+++ Python/main.py
@@ -2,16 +2,12 @@ from DS.node import Node
 from DS.list import List
 from DS.sorts.quick_sort import quicksort
 
+import random
+
 l = List()
-a = Node(100)
-b = Node(200)
-c = Node(300)
 
-l.add(c)
-l.add(b)
-l.add(c)
+for x in range(3):
+    l.add(Node(random.randint(10,100)))
 
-l.all()
-
-
+print(l.all())
 
