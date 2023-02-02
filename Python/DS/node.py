d05cd00da0dfc161359ff33421093038749b3da1
lukehayes <lukepurplehayes@gmail.com>
2023-02-02 14:26:25 +0000

    Removed Python implementations

--- Python/DS/node.py
+++
@@ -1,8 +0,0 @@
-class Node(object):
-
-    def __init__(self, value, next_node = None):
-        self.next = next_node
-        self.value = value
-
-    def __repr__(self):
-        return "Node Value: {}, Next Node: {}\n".format(self.value, self.next)
