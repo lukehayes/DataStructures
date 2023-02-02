cbdde36d3bdad2c1104fb062b166d4bc39862b7a
lukehayes <lukepurplehayes@gmail.com>
2023-02-02 14:27:55 +0000

    Removed C implementations.

--- C/include/node.h
+++
@@ -1,9 +0,0 @@
-#ifndef DS_NODE_H
-#define DS_NODE_H
-
-typedef struct node_t {
-    int value;
-    struct node_t* next;
-} node_t;
-
-#endif // DS_NODE_H
--- C/main.c
+++ C/main.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
-
 
 int main() {
-
-    List* list = CreateList();
-
-    for (int i = 0; i < 10; i++) {
-        ListInsertEnd(list, i);
-    }
-
-    PrintList(list);
-
-    ListRemoveValue(&list, 0);
-    ListRemoveValue(&list, 1);
-    ListRemoveValue(&list, 5);
-
-    PrintList(list);
-
-    DestroyList(list);
 
     return 0;
 }
