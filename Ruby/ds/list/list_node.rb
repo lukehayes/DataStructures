a68afbd3ce0d11f1b02773eb02172aa328fa2d86
Luke Hayes <lukepurplehayes@gmail.com>
2023-02-03 10:15:19 +0000

    Renamed ListNode.next to ListNode.next_node

--- Ruby/ds/list/list_node.rb
+++ Ruby/ds/list/list_node.rb
@@ -4,10 +4,11 @@ module DS
   # Class represents a single Node in a Linked List.
   #
   class ListNode
-    attr_accessor :value, :next
+    attr_accessor :value, :next_node
 
-    def initialize(value = nil)
+    def initialize(value = nil, next_node = nil)
       @value = value
+      @next_node = next_node
     end
   end
 end
