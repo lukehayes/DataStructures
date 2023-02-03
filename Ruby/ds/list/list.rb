0f28edf3ac14295d16f3fd58fc68ae2331a391c1
Luke Hayes <lukepurplehayes@gmail.com>
2023-02-03 10:21:30 +0000

    Added test nodes to List.head.

--- Ruby/ds/list/list.rb
+++ Ruby/ds/list/list.rb
@@ -3,10 +3,25 @@
 module DS
   # A singly linked list.
   class List
+    include Enumerable
+
     attr_accessor :head
 
     def initialize
-      @head = nil
+      @head = ListNode.new(10)
+      @head.next_node = ListNode.new(20)
+      @head.next_node.next_node = ListNode.new(30)
+    end
+
+    def each
+      node = @head
+
+      yield node.value unless node.nil?
+
+      while !node.nil?
+        node = node.next_node
+        yield node.value
+      end
     end
   end
 end
