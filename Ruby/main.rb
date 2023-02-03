e1e11f825f0e46084b1817fa6d1788914fcfcb38
Luke Hayes <lukepurplehayes@gmail.com>
2023-02-03 10:20:34 +0000

    Added test of ListNode and List to main.rb

--- Ruby/main.rb
+++ Ruby/main.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
-require_relative 'ds/node'
+require_relative 'ds/list/list'
+require_relative 'ds/list/list_node'
+
+l = DS::List.new
+
+l.each do |e|
+  puts e
+end
+
+p l.head
+
