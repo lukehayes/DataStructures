9f9a71c9939583028e773bcdb733a6d6e4dca83b
lukehayes <lukepurplehayes@gmail.com>
2023-02-03 18:07:29 +0000

    Removd partially implemented tests

--- Ruby/test/test_list.rb
+++ Ruby/test/test_list.rb
@@ -6,19 +6,14 @@ require_relative '../ds/list/list'
 # List Node Tests.
 #
 class TestList < Minitest::Test
-
   def test_list_is_enumerable
     list = DS::List.new
     assert_instance_of(Enumerable, list)
   end
 
   def test_can_count_number_of_list_elements
-    list = DS::List.new
-    assert_true(0, list.length)
   end
 
   def test_new_list_has_zero_elements
-    list = DS::List.new
-    assert(0, list.size)
   end
 end
