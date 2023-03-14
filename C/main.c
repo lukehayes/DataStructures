93f989e19bde5041a29eab03e9dd1e0abac420e5
lukehayes <lukepurplehayes@gmail.com>
2023-03-14 17:39:50 +0000

    Added linked_list test code to main.

--- C/main.c
+++ C/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
+#include "node.h"
+
+void pn(node_t* node)
+{
+    printf("Node: %i, Next: %p \n", node->value, node->next);
+}
+
+int main()
+{
+    node_t* head = create_node((int*)100);
+    push_value(head, (int*)200);
+
+    for(int i = 3; i <= 5; i++)
+    {
+        push_value(head, (int*)(i*100));
+    }
+
+    push_front(&head, (int*)42);
+    push_front(&head, (int*)213123123);
+    push_front(&head, (int*)563);
+
+    print_node_list(head);
+
+    free(head);
 
     return 0;
 }
