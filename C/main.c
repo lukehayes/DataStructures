7239db926a0724a73fdb2e44a000243bd9718f45
lukehayes <lukepurplehayes@gmail.com>
2023-03-16 18:46:46 +0000

    Updated main.c with latest test code.

--- C/main.c
+++ C/main.c
@@ -12,14 +12,24 @@ int main()
     node_t* head = create_node((int*)100);
     push_value(head, (int*)200);
 
-    for(int i = 3; i <= 5; i++)
-    {
-        push_value(head, (int*)(i*100));
-    }
-
     push_front(&head, (int*)42);
-    push_front(&head, (int*)213123123);
+    push_front(&head, (int*)222);
     push_front(&head, (int*)563);
+
+    // for(int i = 1; i <= 6; ++i)
+    // {
+    //     push_value(head, (int*)(i*100));
+    // }
+
+    push_value(head, (int*)(888));
+    
+    int* res = malloc(sizeof(int));
+    int idx = find_node_value(head, (int*)2888, res);
+    printf("Result %i, index: %i\n", *res, idx);
+
+    free(res);
+
+
 
     print_node_list(head);
 
