fd744bcd456617837b48ade0c947ed260a05e803
lukehayes <lukepurplehayes@gmail.com>
2024-05-10 10:20:47 +0100

    Updated room_print recursive logic.

--- Ruby/ds/tree/room.rb
+++ Ruby/ds/tree/room.rb
@@ -13,10 +13,37 @@ module DS
     attr_accessor :south_west
 
     def initialize
-      @north_east = nil
       @north_west = nil
+      @north_east = nil
       @south_east = nil
       @south_west = nil
+    end
+
+    def print_room(room, depth = 0)
+
+        puts "NW: #{room.north_west}"
+        puts "NE: #{room.north_east}"
+        puts "SW: #{room.south_west}"
+        puts "SE: #{room.south_east}"
+
+        print("Depth: #{depth}")
+        depth.times { print "\t" }
+        print_room(room.north_west, depth + 1) unless room.north_west.nil?
+
+        print("Depth: #{depth}")
+        depth.times { print "\t" }
+        print_room(room.north_east, depth + 1) unless room.north_east.nil?
+
+        print("Depth: #{depth}")
+        depth.times { print "\t" }
+        print_room(room.south_west, depth + 1) unless room.south_west.nil?
+
+        print("Depth: #{depth}")
+        depth.times { print "\t" }
+        print_room(room.south_east, depth + 1) unless room.south_east.nil?
+        #print_room(room.north_east, depth + 1)
+        #print_room(room.south_west, depth + 1)
+        #print_room(room.south,      depth + 1)
     end
 
     class << self
@@ -24,16 +51,16 @@ module DS
       def print_tab(depth)
         depth.times do
           print "\t"
+          puts "Tab Depth #{depth}"
         end
-        STDOUT.flush
       end
 
       def print_room(room, depth)
 
-        puts "NW: #{room.north_west}"
-        puts "NE: #{room.north_east}"
-        puts "SW: #{room.south_west}"
-        puts "SE: #{room.south_east}"
+        #puts "NW: #{room.north_west}"
+        #puts "NE: #{room.north_east}"
+        #puts "SW: #{room.south_west}"
+        #puts "SE: #{room.south_east}"
 
         self.print_room(room.north_west, depth + 1)
         self.print_tab(depth+1)
